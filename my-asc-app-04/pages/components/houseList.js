db80e045f1ddaf312109ac2b4b5600d0f8cac05c
Mohd Naeem <mohd.naeem@ascension.org>
2024-08-14 00:00:30 -0500

    setting up API call using a useEffect hook

--- my-asc-app-04/pages/components/houseList.js
+++ my-asc-app-04/pages/components/houseList.js
@@ -1,64 +1,56 @@
-import { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import HouseRow from "./houseRow";
 
-const housesArray = [
-    {
-        id: 1,
-        address: "123 Frisco way, Friso, TX 75035",
-        country: "USA",
-        price: 135000
-    },
-    {
-        id: 2,
-        address: "456 Plano Pkwy, Plano, TX 75025",
-        country: "USA",
-        price: 175000
-    },
-];
-const HouseList = (props) => {
-    // use of state with an object reference
-    const [houses, setHouses] = useState(housesArray);
+const HouseList = () => {
+  const [houses, setHouses] = useState([]);
 
-    // function to update the state
-    const addHouse = () => {
-        //how to update state using spread syntax
-        setHouses([
-            ...houses, // the original array 
-            {          // adding an element 
-                id: 3,
-                address: "567 Prosper Blvd, Prosper, TX 75065",
-                country: "USA",
-                price: 185000
-            },        
-        ]);
+  useEffect(() => {
+    const fetchHouses = async () => {
+      const response = await fetch("/api/houses");
+      const houses = await response.json();
+      setHouses(houses);
     };
+    fetchHouses();
+  }, []);
 
-    return (
-        <>
-            
-            <div>
-                <h5 className="themeFontColor text-center">
-                    {props.headerText}
-                </h5>
-            </div>
-            <table className="table table-hover">
-                <thead>
-                    <tr>
-                        <th>Address</th>
-                        <th>Country</th>
-                        <th>Asking Price</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {houses.map((h) => <HouseRow key={h.id} house = {h} />)}
-                </tbody>
-            </table>
-            <button className="btn btn-primary" onClick={addHouse}>
-                Add a house
-            </button>
-        </>
+  const addHouse = () => {
+    setHouses([
+      ...houses,
+      {
+        id: 3,
+        address: "32 Valley Way, New York",
+        country: "USA",
+        price: 1000000,
+      },
+    ]);
+  };
 
-    );
+  return (
+    <>
+      <div className="row mb-2">
+        <h5 className="themeFontColor text-center">
+          Houses currently on the market
+        </h5>
+      </div>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>Address</th>
+            <th>Country</th>
+            <th>Asking Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {houses.map((h) => (
+            <HouseRow key={h.id} house={h} />
+          ))}
+        </tbody>
+      </table>
+      <button className="btn btn-primary" onClick={addHouse}>
+        Add
+      </button>
+    </>
+  );
 };
 
 export default HouseList;
